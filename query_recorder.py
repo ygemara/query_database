3046cbfbcdced90aecf4e1f09f722abfbea0a2ac
Yair Gemara <ygemara@gmail.com>
2024-07-25 09:20:36 -0400

    Update query_recorder.py

--- query_recorder.py
+++ query_recorder.py
@@ -128,7 +128,7 @@ def upload_to_google_sheets(new_entry):
     dataframe_headers = database_df.columns.tolist()
 
     if google_sheet_headers != dataframe_headers:
-        st.write("HELLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLO")
+        st.write(dataframe_headers)
         st.error("Column headers do not match!")
     else:
         data = [dataframe_headers] + database_df.values.tolist()
